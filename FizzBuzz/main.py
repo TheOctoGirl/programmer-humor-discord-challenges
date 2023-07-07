2b1e082d7ee0b98fecbda429ad192dbda9f3f2ee
TheOctoGirl <119755793+TheOctoGirl@users.noreply.github.com>
2023-07-06 20:37:34 -0700

    Update main.py

--- FizzBuzz/main.py
+++ FizzBuzz/main.py
@@ -1,16 +1,23 @@
 for i in range(1, 101):
     
-    output = f"{i}: "
+    output = ""
 
     if i % 3 == 0:
         output = output + "Fizz"
+    
     if i % 5 == 0:
         output = output + "Buzz"
+    
     if i % 7 == 0:
         output = output + "Rizz"
+    
     if i % 11 == 0:
         output = output + "Jazz"
+    
     if 120 % i == 0:
         output = output + "Dizz"
+    
+    if output == "":
+        output = i
     
     print(output)
